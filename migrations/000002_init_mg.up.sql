761971369bca8431cf55449bff66d6432d0e3fd5
Elleined <demadegu@gmail.com>
2025-05-22 11:13:48 +0800

    Added refresh_token table

--- migrations/000002_init_mg.up.sql
+++ migrations/000002_init_mg.up.sql
@@ -1,4 +1,6 @@
 CREATE UNIQUE INDEX idx_email ON user(email);
+CREATE INDEX idx_first_last_name ON user (last_name, first_name);
+
 CREATE UNIQUE INDEX idx_name ON emoji(name);
 
 CREATE INDEX idx_create_at ON post(created_at);
---
+++ migrations/000003_init_mg.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS refresh_token (
+    id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME NOT NULL DEFAULT NOW(),
+    token CHAR(36) NOT NULL UNIQUE,
+    expires_at DATETIME NOT NULL,
+    revoked_at DATETIME DEFAULT NULL,
+    user_id BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE INDEX idx_created_at ON refresh_token(created_at);
+CREATE UNIQUE INDEX token ON refresh_token(token);
