339d9e2ef302eb6c70e6b8c7e7defda53fd193d6
Elleined <demadegu@gmail.com>
2025-05-29 09:22:22 +0800

    Added providertypes controller

--- internal/social/provider_type/controller.go
+++ internal/social/provider_type/controller.go
@@ -1,6 +1,10 @@
 package provider_type
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
 
 type (
 	Controller interface {
@@ -40,26 +44,88 @@ func (c ControllerImpl) RegisterRoutes(e *gin.Engine) {
 }
 
 func (c ControllerImpl) save(ctx *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	name := ctx.Query("name")
+
+	id, err := c.service.save(name)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "saved failed! " + err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, id)
 }
 
 func (c ControllerImpl) getById(ctx *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "get by id failed " + err.Error(),
+		})
+		return
+	}
+
+	providerType, err := c.service.getById(id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "get by id failed " + err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, providerType)
 }
 
 func (c ControllerImpl) getAll(ctx *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	providerTypes, err := c.service.getAll()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "get all failed " + err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, providerTypes)
 }
 
 func (c ControllerImpl) update(ctx *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "get by id failed " + err.Error(),
+		})
+		return
+	}
+
+	name := ctx.Query("name")
+	_, err = c.service.update(id, name)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "update failed! " + err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, name)
 }
 
 func (c ControllerImpl) delete(ctx *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "get by id failed " + err.Error(),
+		})
+		return
+	}
+
+	_, err = c.service.delete(id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "delete failed! " + err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusNoContent, nil)
 }
