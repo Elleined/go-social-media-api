809095b72287cafde5f59fffd70c026e713d301c
Elleined <demadegu@gmail.com>
2025-05-29 09:57:12 +0800

    Maintained social repository

--- internal/social/repository.go
+++ internal/social/repository.go
@@ -5,8 +5,6 @@ import "github.com/jmoiron/sqlx"
 type (
 	Repository interface {
 		save(providerTypeId, providerId, userId int, signUpEmail string) (id int64, err error)
-
-		findByUserId(userId int) (Social, error)
 		findByProviderTypeAndId(providerTypeId, providerId int) (Social, error)
 	}
 
@@ -15,22 +13,11 @@ type (
 	}
 )
 
-func (r RepositoryImpl) findByUserId(userId int) (Social, error) {
-	var social Social
-	err := r.Get(&social, "SELECT * FROM social WHERE user_id = ?", userId)
-	if err != nil {
-		return social, err
-	}
-
-	return social, err
-}
-
 func (r RepositoryImpl) save(providerTypeId, providerId, userId int, signUpEmail string) (id int64, err error) {
-	result, err := r.NamedExec("INSERT INTO user_social(provider_type_id, provider_id, user_id, sign_up_email) VALUES (:providerTypeId, :providerId, :userId, :signUpEmail)", map[string]any{
+	result, err := r.NamedExec("INSERT INTO user_social(provider_type_id, provider_id, user_id) VALUES (:providerTypeId, :providerId, :userId)", map[string]any{
 		"providerTypeId": providerTypeId,
 		"providerId":     providerId,
 		"userId":         userId,
-		"signUpEmail":    signUpEmail,
 	})
 	if err != nil {
 		return 0, err
