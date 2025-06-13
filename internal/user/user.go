b45dd426d08ec7fab399fd10afa20b535ee1840b
elleined <demadegu@gmail.com>
2025-06-13 11:32:28 +0800

    change password migration file into default blank string and added fileserver user folder readme

--- internal/user/user.go
+++ internal/user/user.go
@@ -1,14 +1,17 @@
 package user
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type User struct {
-	Id         int       `json:"id" db:"id"`
-	CreatedAt  time.Time `json:"created_at" db:"created_at"`
-	FirstName  string    `json:"first_name" db:"first_name"`
-	LastName   string    `json:"last_name" db:"last_name"`
-	Email      string    `json:"email" db:"email"`
-	Password   string    `json:"-" db:"password"`
-	Attachment string    `json:"attachment" db:"attachment"`
-	IsActive   bool      `json:"is_active" db:"is_active"`
+	Id         int            `json:"id" db:"id"`
+	CreatedAt  time.Time      `json:"created_at" db:"created_at"`
+	FirstName  string         `json:"first_name" db:"first_name"`
+	LastName   string         `json:"last_name" db:"last_name"`
+	Email      string         `json:"email" db:"email"`
+	Password   sql.NullString `json:"-" db:"password"`
+	Attachment sql.NullString `json:"attachment" db:"attachment"`
+	IsActive   bool           `json:"is_active" db:"is_active"`
 }
